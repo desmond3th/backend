e85743eabce3492efc634a1a517dda069a05c119
saurabh <saurabhkumar0184@gmail.com>
2023-11-12 01:15:36 +0530

    datbase

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv"
+import dbConnect from "./db/index.js";
+
+dotenv.config({path: './env'})
+
+dbConnect()
+
+/*
+import express from "express"
+const app = express()
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("Error", (error) => {
+            console.log("Error: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+*/
