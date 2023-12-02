d35f5a4a833e88509f6d8d8a46ba41bb82d940fa
saurabh <saurabhkumar0184@gmail.com>
2023-12-02 23:52:15 +0530

    removed some codes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -12,30 +12,3 @@ const storage = multer.diskStorage({
   
 export const upload = multer({ storage })
 
-
-// alternative to look into
-const multer = require("multer");
-const express = require("express");
-
-const app = express();
-const port = 3000;
-
-const storage1 = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./public/temp");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage1 });
-
-app.post("/upload", upload.single("file"), (req, res) => {
-  // Handle the uploaded file here
-  res.send("File uploaded successfully!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
