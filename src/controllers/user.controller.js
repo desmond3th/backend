3dc4cdc1dbaf0de6f940caeee5c199b2767cb130
saurabh <saurabhkumar0184@gmail.com>
2023-12-06 08:05:44 +0530

    registering user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,19 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 // Defining a route handler for registering a user wrapped with error handling
 const regUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    // get user details from frontend
+    // validate the input 
+    // check if user already exists
+    // check for images and avatar
+    // upload both to cloudinary
+    // create user object (entry in db)
+    // remove refresh token and password from reponse
+    // check if user created
+    // return respone
+
+
+    const {fullname, email, username, password} = req.body
+    
 })
 
 export { regUser }  
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { regUser } from "../controllers/user.controller.js";
 
+import {upload} from "../middlewares/multer.middleware.js"
 
 const route = Router()
 
 // it will take to the register controller
-route.route("/register").post(regUser) // make sure to send the POST status.
+route.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    regUser) // make sure to send the POST status.
 
 export default route
