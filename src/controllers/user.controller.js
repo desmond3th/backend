2b404d8857be4959b2d61725a1adbc6a6d0dd5f2
saurabh <saurabhkumar0184@gmail.com>
2023-12-15 21:40:08 +0530

    user logIn handler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,25 +5,33 @@ import {cloudinaryUpload} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 
-// Defining a route handler for registering a user wrapped with error handling
-const regUser = asyncHandler( async (req, res) => {
-    // get user details from frontend
-    // validate the input 
-    // check if user already exists
-    // check for images and avatar
-    // upload both to cloudinary
-    // create user object (entry in db)
-    // remove refresh token and password from reponse
-    // check if user created
-    // return respone
+/** method for generating access and refresh token **/
+const generateAccessAndRefreshTokens = async (userId) => {
+    try{
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
 
+        user.refreshToken = refreshToken
+        await user.save( {validateBeforeSave : false} )
+
+        return {accessToken, refreshToken}
+
+    } catch(err){
+        throw new ApiError(500, "Couldn't generate Acesss or Refresh token")
+    }
+}
+
+
+/**** Defining a route handler for registering a user ****/
+const regUser = asyncHandler( async (req, res) => {
 
     // Destructuring values from request body
     const { fullname, email, username, password } = req.body;
 
     const fields = [fullname, email, username, password];
     
-    // Checking for empty fields and throw error
+    // Checking for empty fields and throw error i.e. validation
     for (const field of fields) {
         if (!field || !field.trim()) {
             throw new ApiError(400, "Empty fields are not acceptable");
@@ -58,13 +66,11 @@ const regUser = asyncHandler( async (req, res) => {
     const coverImage = await cloudinaryUpload(coverImageLocalPath)
     // console.log('Cover image upload response:', coverImage);
 
-
-
     if(!avatar) {
         throw new ApiError(400, "Avatar file upload failed!");
     }
 
-
+    // create user object (entry in db)
     const user = await User.create({
         fullname,
         avatar: avatar.url,
@@ -80,14 +86,72 @@ const regUser = asyncHandler( async (req, res) => {
         "-password -refreshToken"
     )
 
+    // check if user is created
     if(!checkForUser){
         throw new ApiError(500, "Couldn't register the user");
     }
 
+    //return respone
     return res.status(201).json(
         new ApiResponse(200, checkForUser, "User successfully registered!")
     )
     
 })
 
-export { regUser }  
+
+/*** Defining a route handler for logging In a user ****/
+const loginUser = asyncHandler(async (req, res) => {
+
+    // get data from req body
+    const {username, email, password} = req.body
+
+    // login with username or email
+    if(!email || !username) {
+        throw new ApiError(400, "One of the field is required")
+    }
+
+    // search for the user
+    const user =  await User.findOne({
+        $or : [{username}, {email}]
+    })
+
+    if(!user) {
+        throw new ApiError(402, "User doesn't exist")
+    }
+
+    // check for password
+    const validPassword = await user.isPasswordCorrect(password)
+
+    if(!validPassword) {
+        throw new ApiError(403, "Password Incorrect")
+    }
+
+    // generate access and refresh token
+    const {accessToken, refreshToken} = await 
+    generateAccessAndRefreshTokens(user._id)
+
+    const loggedUser = await User.findById(user._id).
+    select("-password -refreshToken")
+
+    // send these tokens in cookie
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedUser, accessToken, refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+
+})
+
+export { loginUser, regUser }  
