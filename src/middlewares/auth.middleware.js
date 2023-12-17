f89c833d3d9f63acb7aec8eb5a2fd8c1a10ee1aa
saurabh <saurabhkumar0184@gmail.com>
2023-12-17 22:45:13 +0530

    added some comments

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,6 +6,7 @@ import { Jwt } from "jsonwebtoken"
 const verifyJWT = asyncHandler(async(req, res, next) => {
 
 try {
+        // Extract token from cookie or Authorization header
         const token = req.cookie?.accessToken || req.header
         ("Authorization")?.replace("Bearer", "") 
     
@@ -13,8 +14,10 @@ try {
             throw new ApiError(401, "Unauthorized request")
         }
     
+        // Verify the access token using the secret key
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
+        // Find user based on the decoded user ID from the token
         const user = await User.findById(decodedToken?._id)
         .select("-password -refreshToken")
     
@@ -22,6 +25,7 @@ try {
             throw new ApiError(401, "Access Token Invalid")
         }
     
+        // Attach the user to the request for further use in subsequent middleware/routes
         req.user = user
         next()
 
