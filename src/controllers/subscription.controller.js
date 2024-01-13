90dec76e083caba59e2afc23043c373b2c568472
saurabh <saurabhkumar0184@gmail.com>
2024-01-13 23:39:49 +0530

    add toggleSubscription endpoint

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,6 +7,39 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
+    const { channelId } = req.params
+    const { _id: subscriberId } = req.user
+
+    // check if the user is already subscribed
+    const existingSubscription = await Subscription.findOne({
+        subscriber: subscriberId,
+        channel: channelId,
+    })
+
+    if (existingSubscription) {
+        // user is already subscribed; unsubscribe
+        await Subscription.findOneAndDelete({
+        subscriber: subscriberId,
+        channel: channelId,
+        })
+
+        res.status(200)
+        .json(
+            new ApiResponse(200, null, 'Unsubscribed successfully') 
+        )
+
+    } else {
+        // user is not subscribed; subscribe
+        const newSubscription = await Subscription.create({
+        subscriber: subscriberId,
+        channel: channelId,
+        })
+
+        res.status(201)
+        .json(
+            new ApiResponse(201, newSubscription, 'Subscribed successfully') 
+        )
+    }
 })
 
 
