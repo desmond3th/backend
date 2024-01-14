fcdf6e9035bdcf6ada632b8bb2137cbbf9f5a82d
saurabh <saurabhkumar0184@gmail.com>
2024-01-14 20:29:26 +0530

    update route

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { toggleSubscription, getUserChannelSubscribers } from "../controllers/subscription.controller.js"
+import { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels } from "../controllers/subscription.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const route = Router();
@@ -9,5 +9,7 @@ route.use(verifyJWT);
 
 route.route("/c/:channelId").get(getUserChannelSubscribers) 
 route.route("/c/:channelId").post(toggleSubscription)
+
+route.route("/c/:subscriberId").post(getSubscribedChannels)
 
 export default route
