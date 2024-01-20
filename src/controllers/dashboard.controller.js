4eb1a1b2966a46bc3ddfe675c44293665f45cd47
saurabh <saurabhkumar0184@gmail.com>
2024-01-20 11:25:05 +0530

    update dashboard controller with error handling

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -10,68 +10,72 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 /*** Route handler for getting channel stats ***/
 const getChannelStats = asyncHandler(async (req, res) => {
 
-    const totalSubs = await Subscription.aggregate([
-        {
-            $match: { channel : mongoose.Types.ObjectId(req.user._id) },
-        },
-        {
-            $group : {
-                _id : '$channel',
-                subscribersCount : {
-                    $sum : 1
-                }
-            },
-        },
-    ])
-
-    const totalViewsAndTotalVideos = await Video.aggregate([
-        {
-            $match : { owner : mongoose.Types.ObjectId(req.user._id) },
-        },
-        {
-            $group : {
-                _id : '$owner',
-                totalViews : {
-                    $sum : '$views'
+    try {
+            const totalSubs = await Subscription.aggregate([
+                {
+                    $match: { channel : mongoose.Types.ObjectId(req.user._id) },
                 },
-                totalVideos : {
-                    $sum : 1
+                {
+                    $group : {
+                        _id : '$channel',
+                        subscribersCount : {
+                            $sum : 1
+                        }
+                    },
                 },
-            },
-        },
-    ])
-
-    const totalLikes = await Like.aggregate([
-        {
-            $match: { likedBy : mongoose.Types.ObjectId(req.user._id) },
-        },
-        {
-            $group : {
-                _id : null,
-                likesCount : {
-                    $sum : 1
+            ])
+        
+            const totalViewsAndTotalVideos = await Video.aggregate([
+                {
+                    $match : { owner : mongoose.Types.ObjectId(req.user._id) },
                 },
-            },
-        },
-    ])
-
-    if(totalViewsAndTotalVideos.length === 0) {
-        return res.status(200)
-        .json(
-            new ApiResponse(200, {}, "No videos are there on this channel") )
+                {
+                    $group : {
+                        _id : '$owner',
+                        totalViews : {
+                            $sum : '$views'
+                        },
+                        totalVideos : {
+                            $sum : 1
+                        },
+                    },
+                },
+            ])
+        
+            const totalLikes = await Like.aggregate([
+                {
+                    $match: { likedBy : mongoose.Types.ObjectId(req.user._id) },
+                },
+                {
+                    $group : {
+                        _id : null,
+                        likesCount : {
+                            $sum : 1
+                        },
+                    },
+                },
+            ])
+        
+            if(totalViewsAndTotalVideos.length === 0) {
+                return res.status(200)
+                .json(
+                    new ApiResponse(200, {}, "No videos are there on this channel") )
+            }
+        
+            const channelStats = {
+                totalSubs: totalSubs[0]?.subscribersCount || 0,
+                totalViews: totalViewsAndTotalVideos[0]?.totalViews || 0,
+                totalVideos: totalViewsAndTotalVideos[0]?.totalVideos || 0,
+                totalLikes: totalLikes[0]?.likesCount || 0,
+            }
+        
+            return res.status(200)
+            .json(
+                new ApiResponse(201, channelStats, "Channel Stats fetched successfully")
+            )   
+    } catch (error) {
+        throw new ApiError(400, error.message, ": Erorr while fetching channel stats")
     }
-
-    const channelStats = {
-        totalSubs: totalSubs[0]?.subscribersCount || 0,
-        totalViews: totalViewsAndTotalVideos[0]?.totalViews || 0,
-        totalVideos: totalViewsAndTotalVideos[0]?.totalVideos || 0,
-        totalLikes: totalLikes[0]?.likesCount || 0,
-    }
-
-    return res.status(200)
-    .json(
-        new ApiResponse(201, channelStats, "Channel Stats fetched successfully")
-    )
 
 })
 
@@ -79,19 +83,23 @@ const getChannelStats = asyncHandler(async (req, res) => {
 /*** Route handler for getting all the channel's videos ***/
 const getChannelVideos = asyncHandler(async (req, res) => {
 
-    const channelVideos = await Video.find({ owner: req.user.id });
-
-    if(channelVideos.length === 0) {
-        return res.status(200)
-        .json(
-            new ApiResponse(200, {}, "No videos were uploaded on this channel")
-        )
+    try {
+            const channelVideos = await Video.find({ owner: req.user.id });
+        
+            if(channelVideos.length === 0) {
+                return res.status(200)
+                .json(
+                    new ApiResponse(200, {}, "No videos were uploaded on this channel")
+                )
+            }
+        
+            return res.status(201)
+            .json(
+                new ApiResponse(201, channelVideos, "Channel videos fetched successfully")
+            )
+    } catch (error) {
+        throw new ApiError(400, error.message, ": Erorr while fetching channel videos")
     }
-
-    return res.status(201)
-    .json(
-        new ApiResponse(201, channelVideos, "Channel videos fetched successfully")
-    )
 })
 
 
