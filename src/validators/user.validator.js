d46ff9b9be4a9c640b27e080019488e3346dd36e
saurabh <saurabhkumar0184@gmail.com>
2024-02-26 22:53:29 +0530

    add userLoginValidator

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -23,6 +23,13 @@ const userRegisterValidator = () => {
     ];
 }
 
+const userLoginValidator = () => {
+    return [
+        body("email").optional().isEmail().withMessage("Email is invalid"),
+        body("username").optional(),
+        body("password").notEmpty().withMessage("Password is required"),
+    ];
+};
 
-
-export { userRegisterValidator,}
+export { userRegisterValidator,
+        userLoginValidator}
