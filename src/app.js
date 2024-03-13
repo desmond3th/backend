686fe61048b0051cff2046ef62eb83464e917c66
saurabh <saurabhkumar0184@gmail.com>
2024-03-13 17:25:56 +0530

    configure routes for controllers

--- src/app.js
+++ src/app.js
@@ -16,13 +16,27 @@ app.use(cookieParser())
 
 
 // import route
-import route from "./routes/user.route.js";
+import userRoute from "./routes/user.route.js";
+import commentRoute from "./routes/comment.route.js";
+import dashboardRoute from "./routes/dashboard.route.js";
+import healthcheckRoute from "./routes/healthcheck.js";
+import likeRoute from "./routes/like.route.js";
+import playlistRoute from "./routes/playlist.route.js";
+import subscriptionRoute from "./routes/subscription.route.js";
+import tweetRoute from "./routes/tweet.route.js";
+import videoRoute from "./routes/video.route.js";
 
 // declaration
-app.use("/api/v1/users", route)
+app.use("/api/v1/users", userRoute)
+app.use("/api/v1/comment", commentRoute)
+app.use("/api/v1/dashboard", dashboardRoute)
+app.use("api/v1/healthcheck", healthcheckRoute)
+app.use("/api/v1/playlist", playlistRoute)
+app.use("api/v1/like", likeRoute)
+app.use("api/v1/subscription", subscriptionRoute)
+app.use("api/v1/tweet", tweetRoute)
+app.use("api/v1/video", videoRoute)
 
 
-
-// use API testing tool like POSTMAN to verify if API is working.
 
 export { app }
